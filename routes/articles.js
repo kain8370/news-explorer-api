3df14b52d302c14843fc2e6ef12708f9bde8523a
Yuriy <kain8370@yandex.ru>
2020-03-08 17:13:02 +0700

    Внес изменения в валидацию статей

--- routes/articles.js
+++ routes/articles.js
@@ -5,11 +5,11 @@ const { getArticles, createArticle, deleteArticle } = require('../controllers/ar
 router.get('/', getArticles);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    keyword: Joi.string().required().min(2).max(30),
-    title: Joi.string().required().min(2).max(30),
-    text: Joi.string().required().min(2).max(30),
-    date: Joi.string().required().min(2).max(30),
-    source: Joi.string().required().min(2).max(30),
+    keyword: Joi.string().required().min(1).max(60),
+    title: Joi.string().required().min(2),
+    text: Joi.string().required().min(2),
+    date: Joi.string().required().min(2).max(60),
+    source: Joi.string().required().min(2).max(60),
     link: Joi.string().required().regex(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)/),
     image: Joi.string().required().regex(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)/),
   }),
